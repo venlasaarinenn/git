01b6c5b7f1fa80457310108d87b058d692c8d05e
Venla Saarinen <venla.saarinen@edu.turkuamk.fi>
2026-04-29 16:30:53 +0300

    Resolve conflict after stash

--- git_test.py
+++ git_test.py
@@ -3,10 +3,5 @@
 # Description: Code for Git testing purposes
 
 print("I am now more familiar with Git")
-<<<<<<< Updated upstream
-print("This line exists only in development branch before stash")
-=======
 print("This line exists only in development branch before stash")
 ``
-
->>>>>>> Stashed changes
