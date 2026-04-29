455fefc0d9fc99f2244f80e35735c7585ab3d688
Venla Saarinen <venla.saarinen@edu.turkuamk.fi>
2026-04-29 16:22:56 +0300

    Restore stashed change in development

--- git_test.py
+++ git_test.py
@@ -3,4 +3,10 @@
 # Description: Code for Git testing purposes
 
 print("I am now more familiar with Git")
+<<<<<<< Updated upstream
 print("This line exists only in development branch before stash")
+=======
+print("This line exists only in development branch before stash")
+``
+
+>>>>>>> Stashed changes
